ff59b08249dfeeb47dff4e6399339f33f3de7ade
MoringStr <MoringStr010@gmail.com>
2019-09-15 10:24:13 +0800

    初始化

---
+++ S-D/S-D/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  S-D
+//
+//  Created by MoringStar on 2019/9/15.
+//  Copyright © 2019 MoringStar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
